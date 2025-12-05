b6775961d5f61c24ae62cedef38d7b4e12965f00
OmarWaleed2248 <egyptpeko123@gmail.com>
2025-12-05 15:51:09 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,28 +1,22 @@
-# استدعاء المكتبات
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-# رابط الموقع التجريبي
 url = "https://quotes.toscrape.com/"
 
-# جلب صفحة الموقع
 response = requests.get(url)
 
-# تحويل الصفحة ل BeautifulSoup object
 soup = BeautifulSoup(response.text, "html.parser")
 
-# جمع الاقتباسات والمؤلفين
 quotes = soup.find_all("span", class_="text")
 authors = soup.find_all("small", class_="author")
 
-# تجهيز البيانات في قائمة
 data = []
 for q, a in zip(quotes, authors):
     data.append({"Quote": q.text, "Author": a.text})
 
-# تحويل البيانات ل DataFrame وحفظها في CSV
 df = pd.DataFrame(data)
 df.to_csv("quotes.csv", index=False)
 
 print("✅ تم حفظ البيانات في quotes.csv")
+
